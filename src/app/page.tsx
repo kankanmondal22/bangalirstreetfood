992e0060d64b76cc8197efad6a1604b358851348
kankanmondal22 <kankanmondal22@gmail.com>
2026-02-19 00:35:14 +0530

    Refactor navbar component and update layout with new images; remove unused SVG files and clean up global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen bg-amber-50 font-sans text-black">
-            <Navbar />
+        <div className="">
+            
+            
         </div>
     );
 }
