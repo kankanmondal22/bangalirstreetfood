0039791e2958e55b97ea886319dc7844f6a8f1ff
kankanmondal22 <kankanmondal22@gmail.com>
2026-02-19 20:10:08 +0530

    kichuna

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 bg-gray-100 rounded-3xl h-100 m-4">
+        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 bg-gray-100 rounded-lg h-100 m-4">
             Hero
         </div>
     );
