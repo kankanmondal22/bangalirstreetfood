a45526d2a414a8348b5f811b4ae559b21a15bc56
Sanchari Mandal <sancharim2233@gmail.com>
2026-02-19 20:51:45 +0530

    feat: add session management to AdminPage for user authentication and redirection

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,11 @@
+import { useSession } from "@/utils/auth-client";
 import { redirect } from "next/navigation";
 
 export default function AdminPage() {
+  const session = useSession();
+
+  if (!session?.data?.user) {
+    redirect("/admin/login");
+  }
   redirect("/admin/users");
 }
