8e1ae487ae3361846c12ffcaebeaaaea61426b48
Sanchari Mandal <sancharim2233@gmail.com>
2026-03-13 04:35:01 +0530

    fix: adjust z-index for overlay elements in booking package page

--- app/(customer)/booking/[id]/page.tsx
+++ app/(customer)/booking/[id]/page.tsx
@@ -14,7 +14,7 @@ const BookPackage = async ({
   return (
     <div className="bg-gray-100 flex flex-col gap-8 py-12">
       <div className=" mx-auto">
-        <div className="relative">
+        <div className="relative ">
           <Image
             src={packageDetails?.images[0] || "/pahar.jpeg"}
             alt="Package Image"
@@ -23,8 +23,9 @@ const BookPackage = async ({
             loading="eager"
             className="rounded-xl mb-4 overflow-hidden object-cover w-full max-h-96"
           />
+          <div className="absolute z-1 inset-0 bg-linear-to-t from-black/60 to-transparent rounded-xl"></div>
 
-          <div className="absolute bottom-4 left-4 text-white px-4 py-2 rounded">
+          <div className="absolute z-2 bottom-4 left-4 text-white px-4 py-2 rounded">
             <h2 className="text-2xl font-bold">
               {packageDetails?.title || "Package Title"}
             </h2>
