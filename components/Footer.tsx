39d396094913ba0aab7d8f4ce8649502308bad0d
Sanchari Mandal <sancharim2233@gmail.com>
2026-03-15 01:04:33 +0530

    fix: add print:hidden class to Footer and Navbar for print optimization

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const quickLinks = [
 
 const Footer = () => {
   return (
-    <footer className="bg-teal-900 text-gray-100 m-2 mt-12">
+    <footer className="bg-teal-900 text-gray-100 m-2 mt-12 print:hidden">
       <div className="mx-auto max-w-6xl px-4 py-10 sm:px-6 lg:px-8">
         <div className="grid gap-8 text-center sm:grid-cols-2 sm:text-left lg:grid-cols-3">
           <div className="sm:col-span-2 lg:col-span-1">
