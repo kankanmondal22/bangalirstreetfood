623374a9279a80f81b6ee9a527a077b1ed999817
kankanmondal22 <kankanmondal22@gmail.com>
2026-04-08 02:16:57 +0530

    feat: enhance About page with team member display and new features; add statistics and call-to-action sections

--- components/reusable/HomeSections.tsx
+++ components/reusable/HomeSections.tsx
@@ -3,12 +3,17 @@ import React from "react";
 const HomeSections = ({
   className,
   children,
+  style,
 }: {
   className?: string;
   children?: React.ReactNode;
+  style?: React.CSSProperties;
 }) => {
   return (
-    <div className={`mx-auto max-w-6xl p-8 py-24 xl:py-32 ${className}`}>
+    <div
+      className={`mx-auto max-w-6xl p-8 py-24 xl:py-32 ${className}`}
+      style={style}
+    >
       {children}
     </div>
   );
